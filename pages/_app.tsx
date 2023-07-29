49021cc0eef5b9b0a2044554fc5453884bd69839
echoesintospace <echoesintospace@gmail.com>
2023-07-29 17:35:25 +1000

    Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,48 @@ import '@/styles/tailwind.css'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/layoutold'
-// import { Layout } from 'ui/page-directory/layout'
+import Script from 'next/script'
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag"
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+ 
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+ 
+    router.events.on("routeChangeComplete", handleRouteChange);
+ 
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <SessionContextProvider supabaseClient={supabase}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionContextProvider>
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZEC8WC6YR2', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+      />
+      <SessionContextProvider supabaseClient={supabase}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionContextProvider>
+    </>
   )
 }
