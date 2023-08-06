54e49ff1286d043c638248734d9da0121d5c70e1
echoesintospace <echoesintospace@gmail.com>
2023-08-06 15:05:25 +1000

    no need gtag

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,15 +1,15 @@
-export const GA_MEASUREMENT_ID = "G-ZEC8WC6YR2";
+// export const GA_MEASUREMENT_ID = "G-ZEC8WC6YR2";
  
-export const pageview = (url: any) => {
-  window.gtag("config", GA_MEASUREMENT_ID, {
-    page_path: url,
-  });
-};
+// export const pageview = (url: any) => {
+//   window.gtag("config", GA_MEASUREMENT_ID, {
+//     page_path: url,
+//   });
+// };
  
-export const event = ({ action, category, label, value }) => {
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value,
-  });
-};
+// export const event = ({ action, category, label, value }) => {
+//   window.gtag("event", action, {
+//     event_category: category,
+//     event_label: label,
+//     value,
+//   });
+// };
