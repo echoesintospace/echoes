8bef61bd3ca1b836a394b86f553bc7cd0949f0ff
echoesintospace <echoesintospace@gmail.com>
2023-08-20 13:18:14 +1000

    gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,24 @@ import type { AppProps } from 'next/app'
 import Layout from '@/components/layoutold'
 import Script from 'next/script'
 import GoogleAnalytics from "../components/GoogleAnalytics";
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag"
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+ 
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url);
+    };
+ 
+    router.events.on("routeChangeComplete", handleRouteChange);
+ 
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
